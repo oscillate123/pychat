82ec597695fc76c76be4d77759f9f7da03203777
Oscar J Mac <neonode2014@gmail.com>
2019-11-04 15:54:28 +0100

    update

--- pychat_files/node_1.py
+++ pychat_files/node_1.py
@@ -3,13 +3,34 @@ from threading import Thread
 import socket
 
 
+def handler_connection():
+    """Sets up handling for incoming clients."""
+    while True:
+        client, client_address = SERVER.accept()
+        print("%s:%s has connected." % client_address)
+        client.send(bytes("Greetings from the cave! Now type your name and press enter!", "utf8"))
+        addresses[client] = client_address
+        Thread(target=handle_client, args=(client,)).start()
+
+
+
+
+
 connected_nodes = {}
 node_adresses = {}
+
 IP = '127.0.0.1'
 PORT = 6663
 ENC8 = 'utf-8'
+ADDR = (IP, PORT)
+BUFF = 2048
+USERNAME = 'NODE1'
 
-node_socket = socket.socket(AF_INET, SOCK_STREAM)
-node_socket.bind((IP, PORT))
-node_socket.listen(10)
+node = socket.socket(AF_INET, SOCK_STREAM)
+node.bind(ADDR)
+
+if __name__ == "__main__":
+
+	node.listen(10)
+	print("Waiting for connection...")
 
--- pychat_files/node_2.py
+++ pychat_files/node_2.py
@@ -1,1 +1,49 @@
-node_2.py
+from socket import AF_INET, SOCK_STREAM
+from threading import Thread
+import socket
+
+
+def receive():
+	""" Handels receiving """
+	while True:
+		try:
+			msg = node_socket.recv(BUFF)
+			print(msg.decode(ENC8))
+		except OSError:
+			break
+
+def send():
+	""" Handles sending """
+	while True:
+		msg = input(f"{USERNAME} > ")
+		msg = f"{USERNAME} > {msg}"
+
+		if msg == "!quit"
+			node_socket.close()
+			exit()
+		else:
+			node_socket.send(bytes(msg, ENC8))
+
+
+
+connected_nodes = {}
+node_adresses = {}
+
+IP = '127.0.0.1'
+PORT = 6663
+ENC8 = 'utf-8'
+ADDR = (IP, PORT)
+BUFF = 2048
+USERNAME = 'NODE2'
+
+node_socket = socket.socket(AF_INET, SOCK_STREAM)
+node_socket.connect(ADDR)
+
+
+if __name__ == "__main__":
+
+	receive_thread = Thread(target=receive)
+	receive_thread.start()
+
+	send_thread = Thread(target=send)
+	send_thread.start()
